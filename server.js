850acfdc2e530cd5fea2a5b359d430e5f4dd1166
Grace <gracekang.93@gmail.com>
2019-01-11 11:34:30 -0800

    gets all projects

--- server.js
+++ server.js
@@ -3,10 +3,25 @@ const configMiddleware = require('./config/middleware');
 
 const server = express();
 
+const projectsDb = require('./data/helpers/projectModel');
+
 configMiddleware(server);
 
-server.get('/', (req, res) => {
-    res.status(200).send('hello');
+const errorHelper = (res, err) => {
+    res.status(500).json({
+        message: 'There was an error while processing your request.',
+        error: err
+    })
+}
+
+// get all projects
+server.get('/api/projects', async (req, res) => {
+    try {
+        const projects = await projectsDb.get();
+        res.status(200).json(projects);
+    } catch (err) {
+        errorHelper(res, err)
+    }
 });
 
 module.exports = server;
